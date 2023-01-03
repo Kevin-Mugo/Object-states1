ba7af64d8e58a5858cf090a591d488f48d6d3e53
ANZY KENYA <mugokevin900@gmail.com>
2023-01-03 11:36:34 +0300

    #1

--- src/App.js
+++ src/App.js
@@ -13,13 +13,12 @@ export default function App(){
     })
 
     function toggleFavourite(){
-       setContact(prevState => {
-        return{
-          ...prevState ,
+       setContact(prevState => (
+        {
+          ...prevState,
           isFavourite: !prevState.isFavourite
         }
-       }
-       )
+       ))
     }
   
 
